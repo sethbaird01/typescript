1f70212466e8c296172db9b702356e5c36bf0624
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2021-02-22 11:58:07 -0600

    solution

--- resistor-color-duo/resistor-color-duo.test.ts
+++ resistor-color-duo/resistor-color-duo.test.ts
@@ -6,27 +6,27 @@ describe('Resistor Colors', () => {
     expect(resistorColor.value()).toEqual(10)
   })
 
-  xit('Blue and grey', () => {
+  it('Blue and grey', () => {
     const resistorColor = new ResistorColor(['blue', 'grey'])
     expect(resistorColor.value()).toEqual(68)
   })
 
-  xit('Yellow and violet', () => {
+  it('Yellow and violet', () => {
     const resistorColor = new ResistorColor(['yellow', 'violet'])
     expect(resistorColor.value()).toEqual(47)
   })
 
-  xit('Orange and orange', () => {
+  it('Orange and orange', () => {
     const resistorColor = new ResistorColor(['orange', 'orange'])
     expect(resistorColor.value()).toEqual(33)
   })
 
-  xit('Ignore additional colors', () => {
+  it('Ignore additional colors', () => {
     const resistorColor = new ResistorColor(['green', 'brown', 'orange'])
     expect(resistorColor.value()).toEqual(51)
   })
 
-  xit('Throws error when not enough colors', () => {
+  it('Throws error when not enough colors', () => {
     expect(() => new ResistorColor(['green'])).toThrowError(
       'At least two colors need to be present'
     )
--- resistor-color-duo/resistor-color-duo.ts
+++ resistor-color-duo/resistor-color-duo.ts
@@ -1,8 +1,82 @@
+import { stringify } from "querystring";
+
 export class ResistorColor {
   private colors: string[]
 
   constructor(colors: string[]) {
     this.colors = colors
+
+    if(this.colors.length < 2){
+      throw new Error('At least two colors need to be present');
+      
+          }
+          
+    }
+  value = (): number => {
+
+    if(this.colors.length < 2){
+throw new Error('At least two colors need to be present');
+
+    }
+
+    if(this.colors.length >2){
+      for(let ii: number = this.colors.length; ii > 1; ii--){
+        delete this.colors[ii];
+      }
+    }
+
+    let outstring: string = "";
+
+    for(let i: number = 0; i < this.colors.length; i++){
+      console.log("current color: "+this.colors[i]);
+      switch(this.colors[i] ){
+                
+          case "black": 
+        outstring += "0"; 
+        break;
+
+          case "brown": 
+        outstring += "1"; 
+        break;
+
+          case "red": 
+        outstring += "2"; 
+        break;
+
+          case "orange": 
+        outstring += "3"; 
+        break;
+
+          case "yellow": 
+        outstring += "4"; 
+        break;
+
+          case "green": 
+        outstring += "5"; 
+        break;
+
+          case "blue": 
+        outstring += "6"; 
+        break;
+
+        case "violet": 
+        outstring += "7"; 
+        break;
+
+          case "grey": 
+        outstring += "8"; 
+        break;
+
+          case "white": 
+        outstring += "9"; 
+        break;
+
+      }
+    }
+    console.log(outstring);
+    console.log(Number(outstring));
+    return Number(outstring);
+
+  
   }
-  value = (): number => 0
 }
