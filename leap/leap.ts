8e6589c244c9f94f999d5da81e9fdfc80ededf82
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2021-02-23 02:16:20 -0600

    solution

--- leap/leap.ts
+++ leap/leap.ts
@@ -1,5 +1,7 @@
-function isLeapYear(/* Parameters go here */) {
-  // Your code here
+export function isLeapYear(yearIn: number): boolean {
+  if((yearIn%4 == 0 && yearIn %100 !=0) || yearIn%400==0){
+return true;
+  }
+  return false;
 }
 
-export default isLeapYear
