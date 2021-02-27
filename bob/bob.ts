6cee4ffc4c808677de327851b7b483a71ed4c761
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2021-02-27 00:25:10 -0600

    solution + tests enabled
    this one took 1 hour and 20 minutes. jeez

--- bob/bob.ts
+++ bob/bob.ts
@@ -1,6 +1,37 @@
+import { stringify } from "querystring";
+import { isatty } from "tty";
+import { isMainThread } from "worker_threads";
+
 class Bob {
-  hey(/* Parameters go here */) {
-    // Your code here
+  hey(strIn: string): string {
+    
+    //removing whitespace
+    strIn = strIn.replace(/\s+/g, "");
+
+    let isQuestion: boolean = (strIn.charAt(strIn.length - 1) == '?');
+
+    //modified: checks if all LETTERS are caps
+    let strJustASCII: string = strIn.replace(/[^A-z]/g, "");
+    let isAllCaps: boolean = (strJustASCII != "" && strJustASCII == strJustASCII.toUpperCase());
+    
+    if(strIn == ""){
+      return 'Fine. Be that way!';
+    }
+
+    if(isQuestion && !isAllCaps){
+      return 'Sure.';
+    }
+
+    if(isAllCaps && !isQuestion){
+      console.log(strIn);
+      return 'Whoa, chill out!';
+    }
+
+    if(isQuestion && isAllCaps){
+      return "Calm down, I know what I'm doing!";
+    }
+
+    return 'Whatever.';
   }
 }
 
