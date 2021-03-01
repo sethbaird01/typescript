18dd79becad371ed76211cf9d146b9d11f535795
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2021-03-01 01:13:26 -0600

    solution and tests
    Another unfinished solution..

--- robot-name/robot-name.test.ts
+++ robot-name/robot-name.test.ts
@@ -39,16 +39,16 @@ describe('Robot', () => {
     expect(robot.name).toMatch(NAME_RE)
   })
 
-  xit('name is the same each time', () => {
+  it('name is the same each time', () => {
     expect(robot.name).toEqual(robot.name)
   })
 
-  xit('different robots have different names', () => {
+  it('different robots have different names', () => {
     const differentRobot = new Robot()
     expect(differentRobot.name).not.toEqual(robot.name)
   })
 
-  xit('is able to reset the name', () => {
+  it('is able to reset the name', () => {
     const originalName = robot.name
 
     robot.resetName()
@@ -58,7 +58,7 @@ describe('Robot', () => {
     expect(originalName).not.toEqual(newName)
   })
 
-  xit('should set a unique name after reset', () => {
+  it('should set a unique name after reset', () => {
     const NUMBER_OF_ROBOTS = 10000
     const usedNames = new Set()
 
@@ -71,7 +71,7 @@ describe('Robot', () => {
     expect(usedNames.size).toEqual(NUMBER_OF_ROBOTS + 1)
   })
 
-  xit('new names should not be sequential', () => {
+  it('new names should not be sequential', () => {
     const name1 = robot.name
     const name2 = new Robot().name
     const name3 = new Robot().name
@@ -80,7 +80,7 @@ describe('Robot', () => {
     expect(areSequential(name2, name3)).toBe(false)
   })
 
-  xit('names from reset should not be sequential', () => {
+  it('names from reset should not be sequential', () => {
     const name1 = robot.name
     robot.resetName()
     const name2 = robot.name
@@ -92,7 +92,7 @@ describe('Robot', () => {
   })
 
   // This test is optional.
-  xit('all the names can be generated', () => {
+  it('all the names can be generated', () => {
     const usedNames = new Set()
     usedNames.add(robot.name)
 
--- robot-name/robot-name.ts
+++ robot-name/robot-name.ts
@@ -1,15 +1,80 @@
 export default class Robot {
-  constructor() {}
 
+  //global static variables
+public static listAllPossibleNames: string[] = [];
+public static listAllPossible3DigitNumbers: string[] = [];
+public static listAllPossible2LetterStrings: string[] = [];
+
+//name of robot [ object ]
+public robotName: string = "";
+
+//initialize the robot's name when object is created
+  constructor() {
+Robot.releaseNames(); 
+this.resetName();    
+  }
+
+  //returns the number of digits in a number
+  private digits_count(n: number): number {
+    var count = 0;
+    if (n >= 1) ++count;
+  
+    while (n / 10 >= 1) {
+      n /= 10;
+      ++count;
+    }
+  
+    return count;
+  }
+
+  //fills static array with numbers 000-999
+public static generateNumList(): void {
+  let i_string: string = "";
+for (let i = 0; i < 1000; i++) {
+  if(i< 100){
+    i_string = i + "00";
+  }else{
+    i_string = i + "";
+  }
+  Robot.listAllPossible3DigitNumbers[i] = i_string;
+}
+}
+
+//fills static array with strings AA-ZZ
+public static generateStringList(): void {
+    let alphabet: string[] = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    let count: number = 0;
+for (let i = 0; i < alphabet.length ; i++) {
+  for (let a = 0; a < alphabet.length ; a++) {
+  Robot.listAllPossible2LetterStrings[count] = alphabet[i] + alphabet[a];
+  count++;
+}}}
+
+//combines last two functions, so AA000 - ZZ999
+public static generatePossibleNames(): void{
+  let count: number = 0;
+  for (let i = 0; i < Robot.listAllPossible2LetterStrings.length ; i++) {
+    for (let a = 0; a < Robot.listAllPossible3DigitNumbers.length ; a++) {
+    Robot.listAllPossibleNames[count] = Robot.listAllPossible2LetterStrings[i] + Robot.listAllPossible3DigitNumbers[a];
+    count++;
+  }}}
+
+  //returns this robot [ object ]'s name
   public get name(): string {
-    throw new Error('Implement Robot#name')
+    return this.robotName;
   }
 
+  //generates a name based off static variable, then removes the name from array so as not to duplicate it
   public resetName(): void {
-    throw new Error('Implement Robot#resetName')
+    let randomIndex: number = Math.floor(Math.random() * Robot.listAllPossibleNames.length);
+    this.robotName = Robot.listAllPossibleNames.splice(randomIndex, 1)[0];
   }
 
+  //fills the static variables
   public static releaseNames(): void {
-    throw new Error('Implement Robot.releaseNames')
+    this.generateNumList();
+    this.generateStringList();
+    this.generatePossibleNames();
   }
 }
+
