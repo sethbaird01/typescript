d7674c4fa8119bb2362aa695bbe4da7cc3b5f152
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2021-03-08 23:33:03 -0600

    solution and tests
    Given up on this one. The code doesn't work and the concept doesn't make sense to me like the atbash cipher did.

--- simple-cipher/simple-cipher.ts
+++ simple-cipher/simple-cipher.ts
@@ -1,11 +1,47 @@
 class SimpleCipher {
-  encode(/* Parameters go here */) {
-    // Your code here
+  private static alphabet = "abcdefghijklmnopqrstuvwxyz";
+  public key = '';
+
+  constructor(key: string = SimpleCipher.random100chars()) {
+
+    if(key.length < 100){
+      let newkey = "";
+      for(let i =0;i<(150/key.length);i++){
+        //to avoid problems with key being too short
+        newkey += key;
+      }
+      this.key = newkey;
+    }else{
+      this.key = key;
+
+    }
   }
 
-  decode(/* Parameters go here */) {
-    // Your code here
+  private static random100chars(): string{
+    let out= "";
+    for(let i = 0; i<100;i++){
+      out += SimpleCipher.alphabet.charAt(Math.floor(Math.random() * 26));
+    }
+    return out;
   }
+
+  encode(strrIn: string): string {
+    let out = "";
+    for(let i = 0; i<strrIn.length;i++){
+      out += this.key.charAt(SimpleCipher.alphabet.indexOf(strrIn.charAt(i)));
+    }
+    return out;
+  }
+
+  decode(strrIn: string): string {
+    let out = "";
+    for(let i = 0; i<strrIn.length;i++){
+      out += SimpleCipher.alphabet.charAt(this.key.indexOf(strrIn.charAt(i)));
+    }
+    return out;
+  }
+
+
 }
 
 export default SimpleCipher
