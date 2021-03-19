0d378c5c94b970eac64270d7299112990bd09278
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2021-03-19 17:35:56 -0500

    solution + tests

--- gigasecond/gigasecond.ts
+++ gigasecond/gigasecond.ts
@@ -1,7 +1,14 @@
-class Gigasecond {
-  date(/* Parameters go here */) {
-    // Your code here
+export default class Gigasecond {
+  public thisdate: Date;
+  constructor(dateIn: Date){
+    this.thisdate = dateIn;
   }
-}
 
-export default Gigasecond
+  public date(): Date {
+    return this.addMinutes(this.thisdate, (1000000000/60))
+  }
+
+  private addMinutes(date: Date, minutes: number): Date {
+    return new Date(date.getTime() + minutes*60000);
+}
+}
