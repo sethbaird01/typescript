e354aa6bf0c89728abc1f2b54a12ca88b7be8744
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2021-03-25 15:43:07 -0500

    solution + tests

--- collatz-conjecture/collatz-conjecture.ts
+++ collatz-conjecture/collatz-conjecture.ts
@@ -1,6 +1,20 @@
 class CollatzConjecture {
-  static steps(/* Parameters go here */) {
-    // Your code here
+  static steps(numIn: number): number {
+    let steps = 0;
+    if(numIn <= 0){
+      throw new Error('Only positive numbers are allowed');
+    }
+    while(numIn != 1){
+      if(numIn %2 == 0){
+        //n is even
+        numIn /= 2;
+      }else{
+        //n is odd
+        numIn = (numIn*3)+1;
+      }
+      steps++;
+    }
+    return steps;
   }
 }
 
