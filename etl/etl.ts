ab5450662cb5910ebb908704e990070859441bb8
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2021-03-26 21:38:13 -0500

    solution + tests

--- etl/etl.ts
+++ etl/etl.ts
@@ -1,5 +1,27 @@
-function transform(/* Parameters go here */): {
-  // Your code here
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+function transform(inRec: Record<number, string[]>): Record<string, number> {
+  
+  let out: Record<string, number> = {};
+  //ignore this red underline, eslint thinks it should be a const.
+
+for(let i = 0; i<Object.values(inRec).length;i++){
+  //i is traversing through all whole arrays
+  for(let x = 0; x<Object.values(inRec)[i].length;x++){
+    //x is traversing through the array's elements
+    for(let y = 0;y<alphabet.length;y++){
+      //y is traversing through the alphabet
+      if(Object.values(inRec)[i][x].toLowerCase() == alphabet.charAt(y)){
+        const currLetter: string = alphabet.charAt(y);
+        const currPoints = Number(Object.keys(inRec)[i]);
+        out[currLetter] = currPoints;
+      }
+    }
+  }
+  
 }
+return out;
+}
+
 
 export default transform
