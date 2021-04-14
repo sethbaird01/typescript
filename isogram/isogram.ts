a99eee6e35d885f7aae01efde7ca43f556b6a4d7
sethbaird01 <58704551+sethbaird01@users.noreply.github.com>
2021-04-13 23:26:14 -0500

    solution + tests

--- isogram/isogram.ts
+++ isogram/isogram.ts
@@ -1,7 +1,23 @@
-class Isogram {
-  static isIsogram(/* Parameters go here */) {
-    // Your code here
+export default class Isogram {
+  public static alphabet = "qwertyuiopasdfghjklzxcvbnm";
+  
+  static isIsogram(str: string): boolean {
+    let alpha = this.alphabet;
+
+    str = str.replace(/-/g, '');
+    str = str.replace(/ /g, '');
+    str = str.toLowerCase();
+
+    for(let i = 0;i<str.length;i++){
+      if(alpha.includes(str.charAt(i))){
+        const regex = new RegExp(str.charAt(i), 'g')
+        alpha = alpha.replace(regex, '');
+      }else{
+        return false;
+      }
+    }
+
+    return true;
   }
 }
 
-export default Isogram
